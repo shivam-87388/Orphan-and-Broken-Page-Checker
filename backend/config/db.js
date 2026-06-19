90868d97ba80f9f05ba2dfaf12353b6187722e33
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-06-19 21:38:20 +0530

    add

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+const dns = require("node:dns/promises");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
   try {
--- backend/connection.js
+++ backend/connection.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config();
+const dns = require("node:dns/promises");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-mongoose.connect(process.env.MONGO_URL)
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
     .then((result) => {
         console.log('database connected');
     }).catch((err) => {
